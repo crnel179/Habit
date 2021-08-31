de3ce0b7f089a0c460b1ee446013ab8a0369d94e
mariuszlas <mariusz.las@outlook.com>
2021-08-31 19:44:13 +0100

    Added GET and DELETE requests

--- client/static/js/requests.js
+++ client/static/js/requests.js
@@ -1,30 +1,61 @@
-// GET all habits request
-// async function getAllHabits() {
-//
-// }
+const url = "http://localhost:3000/";
 
+// GET all habits
+async function getAllHabits(e) {
+    e.preventDefault();
+
+    const options = {
+        headers: new Headers({'Authorization': localStorage.getItem('token')})
+    }
+    try {
+        const res = await fetch(`${url}habits`, options);
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+        // handleError;
+    }
+}
 
 // POST a new habit
 async function handleHabitForm(e) {
     e.preventDefault()
+
     try {
+        // retrieve data from the form
         let data = Object.fromEntries(new FormData(e.target));
-        data.startDate = new Date;
+        date = new Date;
+        data.startDate = date.toString();
         const options = {
             method: 'POST',
-            headers: new Headers({'Authorization': localStorage.getItem('token')}),
+            headers: new Headers({'Authorization': localStorage.getItem('token')})
             body: JSON.stringify(data)
         }
-        const res = await fetch(url, options);
+        const res = await fetch(`${url}habits`, options);
         // display confirmation message if request was successful
         //
         //
-        // e.target.reset()
     } catch (err) {
-        // handle error
         console.log(err);
+        // handle error
     }
 }
 
 
-// DELETE habit request
+// DELETE a habit
+async function deleteHabit(e, name) {
+    e.preventDefault();
+
+    try {
+        const options = {
+            method: 'DELETE',
+            headers: new Headers({'Authorization': localStorage.getItem('token')})
+        }
+        const res = await fetch(`${url}habits/${name}`, options);
+        const data = res.json();
+        return data;
+    } catch (err) {
+        // handleError
+        console.log(err);
+    }
+}
