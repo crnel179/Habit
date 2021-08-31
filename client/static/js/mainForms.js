f3e22b2db21981732e8aa69431fd55219de18db7
mariuszlas <mariusz.las@outlook.com>
2021-08-31 11:28:28 +0100

    Renamed forms.js to mainForms.js

--- client/static/js/mainForms.js
+++ client/static/js/mainForms.js
@@ -1,10 +1,13 @@
-//get the button elements
+//get the button elements for habit forms
 const openModalBtn = document.querySelector("#add-habit-btn");
 const closeModalBtn = document.querySelector('.close-modal')
 const habitModal = document.querySelector("#add-habit-modal");
+const newHabitForm = document.querySelector('#add-habit-form');
 
+//attach event listeners for habit forms
 openModalBtn.addEventListener('click', e => showModal(e, true))
 closeModalBtn.addEventListener('click', e => showModal(e, false))
+newHabitForm.addEventListener('submit', e => handleHabitForm(e));
 
 function showModal(e, show) {
     e.preventDefault();
@@ -14,4 +17,14 @@ function showModal(e, show) {
     } else {
         habitModal.classList.add("d-none")
     }
+}
+
+
+let dummyHabit = {
+    name: "habit",
+    frequency: 1,
+    priority: false,
+    tag: 'health',
+    startDate: '20/02/2012',
+    highestStreak: 14
 }
