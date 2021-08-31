3ea210c54ac4f498a8793a5b3b84ea59f4b12eb6
jasminappleby <60107486+jasminappleby@users.noreply.github.com>
2021-08-31 14:38:43 +0100

    started writing habit controller test suite. added beforeEach and afterAll

---
+++ api/tests/unit/controllers/habits.spec.js
@@ -0,0 +1,13 @@
+const habitController = require('../../../controllers/habits')
+//const Habit = require('../../../models/Habit') need this to be added
+
+const mockSend = jest.fn();
+const mockJson = jest.fn();
+const mockStatus = jest.fn(code => ({ send: mockSend, json: mockJson, end: jest.fn() }))
+const mockRes = { status: mockStatus }
+
+describe('habits controller', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    afterAll(() => jest.resetAllMocks());
+})
