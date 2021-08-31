4a877d278bb4c4950d62b2ca4a3d915e6ced8e1a
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-08-31 17:29:14 +0100

    added update method to Habit class

--- api/model/Habit.js
+++ api/model/Habit.js
@@ -29,7 +29,7 @@ class Habit {
     static findByName(name) {
         return new Promise(async (resolve, reject) => {
             try {
-                const db = await init()
+                const db = await init();
                 const habitData = await db.collection.find({ user_email: user }, { habits: 1, _id: 0 }).toArray();
                 //need to search through array for habit object with matching name
                 const habit;
@@ -47,21 +47,67 @@ class Habit {
         })
     }
 
+
+    // static update(name) {
+    //     return new Promise(async (resolve, reject) => {
+    //         try {
+    //             const db = await init();
+    //             const habitData = await db.collection.find()
+    //         }
+    //     })
+    // }
+
+    //----------------------------------alternative update/ deletes method-----------------------------//
+
+
+    static update(name, body) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const db = await init();
+                const update = { $set: { [`habits.${name}`]: body } };
+                const options = { returnNewDocument: true };
+                const updatedHabits = db.collection.findOneAndUpdate({ user_email: user }, update, options);
+                resolve(updatedHabits)
+            }
+            catch (err) {
+                console.log(err)
+                reject('error: could not update')
+            }
+        })
+    }
+
     static destroy(name) {
         return new Promise(async (resolve, reject) => {
             try {
                 const db = await init();
-                const habitData = await db.collection.find({user_email: user}, {habits: 1, _id: 0}).toArray();
-                //need to find the correct habit to pop.
-                const habitIndex = habitData.find(habit => habit.name == name);
-                newHabitArray = habitData.splice(habitIndex, 1);
-                const removed = await db.collection.updateOne({user_email: user}, {$set: {habits: newHabitArray}})
+                const removed = db.collection.remove({ user_email: user }, { $unset: { [`habits.${name}`]: "" } });
                 resolve(removed)
             }
-            catch(err){
+            catch (err) {
                 console.log(err)
                 reject('error in deleting this habit')
             }
         })
     }
 }
+
+    //---------------------------------------------------------------------------------------//
+
+//     static destroy(name) {
+//         return new Promise(async (resolve, reject) => {
+//             try {
+//                 const db = await init();
+//                 const habitData = await db.collection.find({ user_email: user }, { habits: 1, _id: 0 }).toArray();
+//                 //need to find the correct habit to pop.
+//                 const habitIndex = habitData.find(habit => habit.name == name);
+//                 newHabitArray = habitData.splice(habitIndex, 1);
+//                 const removed = await db.collection.updateOne({ user_email: user }, { $set: { habits: newHabitArray } })
+//                 resolve(removed)
+//             }
+//             catch (err) {
+//                 console.log(err)
+//                 reject('error in deleting this habit')
+//             }
+//         })
+//     }
+//
