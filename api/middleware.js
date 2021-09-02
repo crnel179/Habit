f4226dc53a0757b80c118b920a38505be34ff1be
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-02 13:30:34 +0100

    updated middleware

--- api/middleware.js
+++ api/middleware.js
@@ -4,10 +4,11 @@ const tokenVerified = async (req, res, next) => {
     try {
         const options = {
             method: 'POST',
-            headers: { 'content-type': 'application/json' },
+            headers: { 'Content-Type': 'application/json' },
             body: { token: req.headers["Authorization"] }
         }
         const resp = await fetch(`${url}/tokens/authenticate`, options)
+        console.log(resp)
         if (resp.status !== 200) {
             throw new Error()
         }
