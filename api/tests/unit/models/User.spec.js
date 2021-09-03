18a0e0b56abfa68a47d108e35bf7f98e32fd5724
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-09-03 09:23:10 +0100

    16 tests, 3 pass lol. im so sad

---
+++ api/tests/unit/models/User.spec.js
@@ -0,0 +1,33 @@
+const User = require('../../../model/Habit');
+const { MongoClient } = require ('mongodb');
+jest.mock('mongodb');
+
+const db = require('../../../dbConfig');
+
+describe('User', () => {
+    beforeEach(() => jest.clearAllMocks())
+
+    afterAll(() => jest.resetAllMocks())
+
+    describe('all', () => {
+        test('it resolves with user on successful db query', async () => {
+            jest.spyOn(db, 'collection')
+                .mockResolvedValueOnce({rows: [{}, {}, {}]})
+            const all = await User.all;
+            expect(all).toHaveLength(1)
+        })
+    });
+
+    describe('findByName', () => {
+        test('it resolves with user on successful db query', async () => {
+            let userData = { name: 'user' }
+            jest.spyOn(db, 'collection')
+                .mockResolvedValueOnce({rows: [ userData ] });
+            const result = await User.findByName('user');
+            expect(result).toBeInstanceOf(User)
+        })
+    });
+
+    
+    
+})
